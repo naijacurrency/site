5fb63587cc1c5cd66f161be3ecd3e06370eeba15
naijacurrency <95143920+naijacurrency@users.noreply.github.com>
2021-12-03 07:43:35 +0100

    Add files via upload

---
+++ js/uselater.js
@@ -0,0 +1,4 @@
+
+var usedlaterscript = document.createElement("script");
+usedlaterscript.src = "https://cdn.jsdelivr.net/npm/chart.js@3.6.0/dist/chart.min.js";
+document.body.appendChild(usedlaterscript);
