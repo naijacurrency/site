f16d7c3cc8aa99690b6f58c8efe9afbb76cc3f44
naijacurrency <95143920+naijacurrency@users.noreply.github.com>
2022-09-25 22:41:49 +0100

    Update 20000-to-naira.js

--- js/20000-to-naira.js
+++ js/20000-to-naira.js
@@ -421,7 +421,7 @@ const myChart = new Chart(ctx, {
      document.getElementById("mm5").innerHTML = m2;
     document.getElementById("mm6").innerHTML = m1;
   
-    console.log(data[0].rates.NGN);
+    console.log("1:"+data[0].rates.NGN);
 		
     document.getElementById("mr1").innerHTML = data[0].rates.NGN;
     document.getElementById("mr2").innerHTML = data[1].rates.NGN;
@@ -582,7 +582,7 @@ const myChart = new Chart(ctx, {
      document.getElementById("mm5").innerHTML = m2;
     document.getElementById("mm6").innerHTML = m1;
   
-    
+    console.log("2:"+data[0].rates.NGN);
     document.getElementById("mr1").innerHTML = data[0].rates.NGN;
     document.getElementById("mr2").innerHTML = data[1].rates.NGN;
      document.getElementById("mr3").innerHTML = data[2].rates.NGN;
