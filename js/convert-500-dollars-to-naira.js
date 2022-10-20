4c15b71d2827f47ab917d90bdc4c5215db54f543
naijacurrency <95143920+naijacurrency@users.noreply.github.com>
2022-10-20 15:34:12 +0100

    Update convert-500-dollars-to-naira.js

--- js/convert-500-dollars-to-naira.js
+++ js/convert-500-dollars-to-naira.js
@@ -370,7 +370,7 @@ var rate2d = numr.toFixed(2);
  //display pounds rate   
 	
 var gbrate = parseFloat(data[9].price);
-var gate = gbrate+(0.07*gbrate);
+var gate = gbrate+(0.0*gbrate);
 console.log(gate);
 console.log(blrate);
 		
@@ -550,7 +550,7 @@ var conbdisplay = conbdispla.toLocaleString();
 	
 	    //display gbp rate
 	  var gbrate = parseFloat(data[9].price);
-var gate = gbrate+(0.07*gbrate);
+var gate = gbrate+(0.0*gbrate);
 console.log(gate);
 	    console.log(blrate);
 		
