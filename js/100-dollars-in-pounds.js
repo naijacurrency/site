d2379cce717d8c5e956a7c41de747825f01a0863
naijacurrency <95143920+naijacurrency@users.noreply.github.com>
2023-10-05 20:44:06 +0100

    Add files via upload

--- js/100-dollars-in-pounds.js
+++ js/100-dollars-in-pounds.js
@@ -7,26 +7,75 @@ document.getElementById("conv").addEventListener("click",function(){
 	var currency=basecurrency.value;
 	var from=firstinput.value;
 	var to=covertcurrency.value;
-	var requestURL='https://api.exchangerate.host/convert?from='+currency+'&to='+to+'&amount='+from+'';
+//	var requestURL='https://api.exchangerate.host/convert?from='+currency+'&to='+to+'&amount='+from+'';
+	var requestURL='https://v6.exchangerate-api.com/v6/a779c2ab746dc1471f4cf977/pair/'+currency+'/'+to;
 	var request=new XMLHttpRequest();
 	
 	request.open('GET',requestURL);request.responseType='json';request.send();request.onload=function(){
 		var response=request.response;
-		var rate=parseFloat(response.info.rate);
-		var result=parseFloat(from)*rate;function format(n,sep,decimals){sep=sep||".";decimals=decimals||2;return n.toLocaleString().split(sep)[0]
+		//console.log(response);
+		var rate=parseFloat(response.conversion_rate);
+		var result=parseFloat(from)*rate;
+		function format(n,sep,decimals){sep=sep||".";
+		decimals=decimals||2;
+		return n.toLocaleString().split(sep)[0]
 +sep
 +n.toFixed(decimals).split(sep)[1];}
 
-resultinput.innerHTML=format(result/100);}
+resultinput.innerHTML=format(result/100);
+}
+
 }else{document.getElementById("notification").innerHTML="please complete all information required";}});
+
+
 
 function monthName(mon){return['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'][mon-1];}
 
-document.addEventListener("DOMContentLoaded",function(){setTimeout(function(){var tag=document.createElement("script");tag.src="https://www.googletagmanager.com/gtag/js?id=G-XFZL8723RQ";document.getElementsByTagName("head")[0].appendChild(tag);var tag=document.createElement("script");tag.src="js/gascript.js";document.getElementsByTagName("head")[0].appendChild(tag);var tag=document.createElement("script");tag.src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=jetli004";
+document.addEventListener("DOMContentLoaded",function(){
+	
+	setTimeout(function(){var tag=document.createElement("script");
+tag.src="https://www.googletagmanager.com/gtag/js?id=G-XFZL8723RQ";
+document.getElementsByTagName("head")[0].appendChild(tag);
+var tag=document.createElement("script");
+tag.src="js/gascript.js";
+document.getElementsByTagName("head")[0].appendChild(tag);
+var tag=document.createElement("script");
+tag.src="https://s7.addthis.com/js/300/addthis_widget.js#pubid=jetli004";
 
 document.getElementsByTagName("body")[0].appendChild(tag);
 
-if(localStorage.getItem("all_data_100")){console.log("data found");var p_data=JSON.parse(localStorage.getItem("all_data_100"));var p_timestamp=p_data[7].timestamp;var n_timestamp=Date.now();var d_timestamp=(n_timestamp-p_timestamp)/100/60/60;if(d_timestamp<1){loadLS();}else{loadApi();}}else{loadApi();}},3100);});function format(n,sep,decimals){sep=sep||".";decimals=decimals||2;return n.toLocaleString().split(sep)[0]
+if(localStorage.getItem("all_data_100")){
+	console.log("data found");
+var p_data=JSON.parse(localStorage.getItem("all_data_100"));
+var p_timestamp=parseFloat(p_data[7].timestamp);
+var n_timestamp=Date.now();
+var d_timestamp=(parseFloat(n_timestamp)-p_timestamp)/100/60/60;
+
+
+if(d_timestamp < 1){
+	console.log(d_timestamp);
+	//its not up to an hr since the last load
+
+	loadLS();
+} else {
+
+	console.log(d_timestamp);
+	//timestamp differenc is more than 1 hr
+
+	loadApi();
+}
+} else {
+//data is not available
+
+loadApi();
+}  
+
+ }, 3500);
+});
+
+
+
+function format(n,sep,decimals){sep=sep||".";decimals=decimals||2;return n.toLocaleString().split(sep)[0]
 +sep
 +n.toFixed(decimals).split(sep)[1];}
 
@@ -54,34 +103,61 @@ var today1=previous1months.toISOString().slice(0,10);
 var m11=today1.split("-");var m1=monthName(m11[1]);
 var currency="USD";var from=100;
 var to="GBP";
-	var url1="https://api.exchangerate.host/"+today6+"?base=USD&symbols=USD,GBP&amount=100";
+
+
+var app_id = "2811fd1ec0024160ab4d4f63316e1730";
+
+var url1="https://openexchangerates.org/api/historical/"+today6+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url2="https://openexchangerates.org/api/historical/"+today5+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url3="https://openexchangerates.org/api/historical/"+today4+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url4="https://openexchangerates.org/api/historical/"+today3+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url5="https://openexchangerates.org/api/historical/"+today2+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url6="https://openexchangerates.org/api/historical/"+today1+".json?app_id="+app_id+"&symbols=NGN,GBP";
+var url7="https://openexchangerates.org/api/latest.json?app_id=2811fd1ec0024160ab4d4f63316e1730&symbols=NGN,GBP";
+
+/*
+var url1="https://api.exchangerate.host/"+today6+"?base=USD&symbols=USD,GBP&amount=100";
 var url2="https://api.exchangerate.host/"+today5+"?base=USD&symbols=USD,GBP&amount=100";
 var url3="https://api.exchangerate.host/"+today4+"?base=USD&symbols=USD,GBP&amount=100";
 var url4="https://api.exchangerate.host/"+today3+"?base=USD&symbols=USD,GBP&amount=100";
 var url5="https://api.exchangerate.host/"+today2+"?base=USD&symbols=USD,GBP&amount=100";
 var url6="https://api.exchangerate.host/"+today1+"?base=USD&symbols=USD,GBP&amount=100";
 var url7="https://api.exchangerate.host/convert?from="+currency+"&to="+to+"&amount="+from+"'";
+*/
 var url8="https://api.binance.com/api/v3/ticker/price?symbol=GBPUSDT";
 var url9="https://api.allorigins.win/get?url=https://www.cbn.gov.ng/rates/outputExchangeRateJSN.asp";
 var url10="https://api.binance.com/api/v3/ticker/price?symbol=GBPUSDT";
 
 Promise.all([fetch(url1),fetch(url2),fetch(url3),fetch(url4),fetch(url5),fetch(url6),fetch(url7),fetch(url8),fetch(url9),fetch(url10)])
-.then(function(responses){return Promise.all(responses.map(function(response){return response.json();}));})
-	.then(function(data){var all_data_100=data;
+.then(function(responses){
+		return Promise.all(responses.map(
+			function(response){
+							return response.json();}));
+						}).then(function(data){
+			var all_data_100=data;
 var timestamp=Date.now()
 var addtime={timestamp:timestamp};
 all_data_100.push(addtime);
 localStorage.setItem("all_data_100",JSON.stringify(all_data_100));
 
 var blrate=parseFloat(data[7].price);
-
+console.log(blrate);
+var bl = (1/blrate).toFixed(2);
+console.log(bl);
 var sellrate=blrate+710;
 var conbdispla=100/blrate;
 var conbdisplay=conbdispla.toLocaleString();
 
 document.getElementById("selldollarsrate").innerHTML=sellrate;
-var n=data[6].result;
-var con2d=n.toLocaleString();document.getElementById("con").innerHTML=con2d;document.getElementById("conb").innerHTML=conbdisplay;
+
+console.log(data);
+
+var n=data[6].rates.GBP*100;
+//console.log(n);
+var con2d=n.toLocaleString();
+
+document.getElementById("con").innerHTML=con2d;
+document.getElementById("conb").innerHTML=conbdisplay;
 
 const str=JSON.stringify(data[8]);
 const[first,...rest]=str.split('sellingrate');
@@ -90,9 +166,9 @@ const str2=rest;
 const[first1,...rest1]=str.split(',');
 var newt=rest1[3].replace("sellingrate","");
 
-const str3=newt;
+//const str3=newt;
 const[first2,...rest2]=newt.split(':');
-console.log(rest2);
+//console.log(rest2);
 const crate=rest2[0].slice(0,-1);
 console.log(rest2[0]);			     
 var concrate=parseFloat(crate)*100;
@@ -101,19 +177,21 @@ var con2rated=concrate.toLocaleString();
 
 //document.getElementById("conc").innerHTML=con2rated;
 			     
-var numr=data[6].info.rate;
+var numr=data[6].rates.GBP;
 			     console.log(numr);
 var rate2d=numr.toFixed(2);
 			     console.log(rate2d);
 document.getElementById("rat").innerHTML=rate2d;
 			     
-document.getElementById("ratb").innerHTML=blrate;
+document.getElementById("ratb").innerHTML=bl;
 			     
 //document.getElementById("ratc").innerHTML=crate;
 
 var gbrate=parseFloat(data[9].price);
 
-var gate=gbrate+(0.0*gbrate);console.log(gate);console.log(blrate);
+var gate=gbrate+(0.0*gbrate);
+console.log(gate);
+console.log(blrate);
 var gbp2GBP=gate*blrate;var grate=gbp2GBP.toFixed(2);
 document.getElementById("pou").innerHTML=grate;
 const ctx=document.getElementById('myChart').getContext('2d');
@@ -130,6 +208,7 @@ const myChart=new Chart(ctx,{type:'line',data:{
 	document.getElementById("mm4").innerHTML=m3;
 	document.getElementById("mm5").innerHTML=m2;
 	document.getElementById("mm6").innerHTML=m1;
+
 	document.getElementById("mr1").innerHTML=data[0].rates.GBP;
 	document.getElementById("mr2").innerHTML=data[1].rates.GBP;
 	document.getElementById("mr3").innerHTML=data[2].rates.GBP;
@@ -160,22 +239,32 @@ var currency="USD";
 var from=100;
 var to="GBP";
 var blrate=parseFloat(data[7].price);
-console.log(blrate);var sellrate=blrate-25;
-var conbdisplay=100 / blrate;console.log(conbdisplay);
+console.log(blrate);
+var sellrate=blrate-25;
+var conbdisplay=100 / blrate;
+console.log(conbdisplay);
+
 document.getElementById("selldollarsrate").innerHTML=format(sellrate);
-var n=data[6].result;var con2d=n.toLocaleString();
+
+var n=data[6].rates.GBP*100;
+var con2d=n.toLocaleString();
+
 document.getElementById("con").innerHTML=con2d;
 document.getElementById("conb").innerHTML=format(conbdisplay);
-document.getElementById("conc").innerHTML=format(data[6].result);
-document.getElementById("rat").innerHTML=format(data[6].info.rate);
+document.getElementById("conc").innerHTML=format(data[6].rates.GBP);
+document.getElementById("rat").innerHTML=format(data[6].rates.GBP);
+
 var conbdispla=blrate*100;
 var conbdisplay=conbdispla.toLocaleString();
 //document.getElementById("ratb").innerHTML=conbdisplay;
-document.getElementById("ratc").innerHTML=format(data[8].info.rate);
+document.getElementById("ratc").innerHTML=format(data[8].rates.GBP);
 var gbrate=parseFloat(data[9].price);
-var gate=gbrate+(0.0*gbrate);console.log(gate);
-console.log(blrate);var gbp2GBP=gate*blrate;
-var grate=gbp2GBP.toFixed(2);document.getElementById("pou").innerHTML=grate;
+var gate=gbrate+(0.0*gbrate);
+console.log(gate);
+console.log(blrate);
+var gbp2GBP=gate*blrate;
+var grate=gbp2GBP.toFixed(2);
+document.getElementById("pou").innerHTML=grate;
 
 const ctx=document.getElementById('myChart').getContext('2d');
 
