9e292639370889b40a283ca41466d699262041a9
naijacurrency <95143920+naijacurrency@users.noreply.github.com>
2023-11-19 00:42:16 +0000

    Add files via upload

--- js/loadarticle.js
+++ js/loadarticle.js
@@ -1,10 +1,16 @@
+var tag = document.createElement("script");
+tag.src = "../js/modal_load.js";
+document.getElementsByTagName("body")[0].appendChild(tag);
+
+
+
 var tmenu = '<a class="uk-navbar-brand" href="/index.html"><span class="customspan">NaijaCurrencyConverter</span></a><div class="uk-container uk-container-center"><ul class="uk-navbar-nav uk-hidden-small"><li><a href="../index.html">Welcome</a></li><li><a href="../aboutus.html">About Us</a></li><li><a href="../#qc">Quick Calculators</a></li><li><a href="../contactus.html">Contact us</a></li></ul><a href="../#tm-offcanvas" class="uk-visible-small" data-uk-offcanvas=""><span><i class="fas fa-bars fa-lg" style="margin-top:12px"></i></span></a></div>';
 
 var qcc = '<ul class="uk-grid custom-body" data-uk-grid-margin><li class="uk-width lwidht"><a href="../convert-500-dollars-to-naira.html">Convert 500 dollars to Naira</a></li><li class="uk-width lwidht"><a href="../convert-naira-to-dollar.html">Convert naira to dollar</a></li><li class="uk-width lwidht"><a href="../20000-dollars-to-naira.html">20000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../200000-dollars-to-naira.html">200000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../convert-500-dollars-to-naira.html">Convert 500 dollars to naira</a></li><li class="uk-width lwidht"><a href="../50-dollars-to-naira.html">50 dollars to naira</a></li><li class="uk-width lwidht"><a href="../5000-dollars-to-naira.html">5000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../convert-2000-dollars-to-naira.html">Convert 2000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../3000-dollars-to-naira.html">3000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../canadian-dollar-to-naira.html">canadian dollar to naira</a></li><li class="uk-width lwidht"><a href="../convert-200-dollars-to-naira.html">Convert 200 dollars to naira</a></li><li class="uk-width lwidht"><a href="../1-euro-to-naira.html">1 euro to naira</a></li><li class="uk-width lwidht"><a href="../2000000-dollars-to-naira.html">2000000 dollars to naira</a></li><li class="uk-width lwidht"><a href="../convert-btc-to-usd.html">Convert btc to usd</a></li><li class="uk-width lwidht"><a href="../200000-naira-to-USD.html">200000 naira to USD</a></li><li class="uk-width lwidht"><a href="../1000-dollars-to-naira.html">1000 dollars to naira</a></li></ul>';
 
 var calcu = "";
 
-var offers = '<table style="width:90%;max-width:600px"><tr><td style="padding:10px"><span style="color:blue; font-weight:bold;">Pay USD, receive in Naira Bank account</span> <br> Payoneer, USD bank transfer, USD Cards payments accepted. <br><b><a href="https://wa.me/2348032367641?text=I%20am%20interested%20in%20exchanging%20Some%20dollars">Chat now to start</a></b></td></tr><tr><td style="padding:10px"><span style="color:blue; font-weight:bold;">Buy Pound Sterling with Naira</span>.<br> We give unbeliveable rates. Direct deposit into UK bank account only. Minimum 100GBP, Maximum 1000 GBP. 2 days maximum bank transfer time. Escrow services available.<br><b><a href="https://wa.me/2348032367641?text=I%20am%20interested%20in%20buying%20Some%20pounds">Chat now to start</a></b></td> </tr> <tr> <td style="padding:10px"><span style="color:blue; font-weight:bold;">Open a Multicurrency Account - USD, GBP, EUR for FREE with a Debit card </span>.<br> Send money cheaper and easier than old-school banks, Use your card in more than 175 countries and withdraw money from 2.3 million ATMs. Tap or swipe anywhere — and use Google Pay and Apple Pay in supported countries. <br><b><a href="https://bit.ly/dollarstransfernow">Learn more</a></b></td> </tr></table>';
+var offers = '<table style="width:90%;max-width:600px"><tr><td style="padding:10px"><span style="color:blue; font-weight:bold; font-size:18px">How to buy Dollars, Pounds and any foreign Currency from Home FAST</span>.<br><span style="font-size:18px">Learn how to exchange currencies by yourself at lower exchange rates </span> <br><a href=""><button id="myBtn">Instant Access</button></a></td></tr><div id="myModal" class="modal"><div class="modal-content"><iframe width="540" height="535" src="https://b5030a47.sibforms.com/serve/MUIFAIJ1d3n1KOv-zI-IjCEUHzj5tCSlu9RSXkKMR71yJND53_tiuwbIxIKA0SSWYDM5kZSYI_Oaf1156qiYBFFFMHm0OAnqU9Hl4wjPJllooCzLpkGuAuIbYOCf3t4P30mcguAaX3rSwWxA34bqwG7gvKZPLttHKrWPjX0K60aJrWamsrCisSDQEFiAhANnihn1W2TGdY8yCbzY" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe></div></div><tr> <td style="padding:10px"><span style="color:blue; font-weight:bold;">Open a Multicurrency Account - USD, GBP, EUR for FREE with a Debit card </span>.<br> Send money cheaper and easier than old-school banks, Use your card in more than 175 countries and withdraw money from 2.3 million ATMs. Tap or swipe anywhere — and use Google Pay and Apple Pay in supported countries. <br><a href="https://bit.ly/dollarstransfernow">Learn more</a></td> </tr></table>';
 
 
 var bmenu = '<ul class="uk-nav uk-nav-offcanvas" data-uk-nav="{ multiple: true }"><li class="uk-parent" aria-expanded="false"><a href="../#">Menu</a></li><li class="uk-nav-header"><a href="../index.html">Home</a></li><li class="uk-nav-header"><a href="../aboutus.html">About Us</li><li class="uk-nav-header"><a href="../#qc">Quick Calculators</a></li><li class="uk-nav-header"><a href="../contactus.html">Contact us</li><li class="uk-nav-header"><a href="../disclaimer.html">Data Disclaimer</a></li><li class="uk-nav-header"><a href="../privacy-policy.html">Privacy Policy</a></li></ul>';
@@ -25,6 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
 	document.getElementById("ofr").innerHTML = offers;
 	//load bottom menu
 	document.getElementById("nf").innerHTML = bottom_menu;
+
+
+	
+		//add google analytics
+	  var tag = document.createElement("script");
+	  tag.src = "https://www.googletagmanager.com/gtag/js?id=G-XFZL8723RQ";
+	  document.getElementsByTagName("head")[0].appendChild(tag);
+	  
+	  var tag = document.createElement("script");
+	  tag.src = "../js/gascript.js";
+	  document.getElementsByTagName("head")[0].appendChild(tag);
+	  
+
 	
 });
 
